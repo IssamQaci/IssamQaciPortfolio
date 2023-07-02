a55366f7036ddca5956072af93aa3704189c1941
IssamQaci <128149288+IssamQaci@users.noreply.github.com>
2023-07-02 13:02:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -15,106 +15,79 @@ function getmouseposition(event){
 
 }   
 
-
+op = 0;
 op1 = 0;
 op2 = 0;
 op3 = 0;
 op4 = 0;
-function func1(){
-    inter = setInterval(() => {
-        document.getElementById("frontend").style.boxShadow = "rgba(48, 173, 67, 0.4) 5px 5px, rgba(48, 173, 67, "+ op1 +") 10px 10px, rgba(48, 173, 67, "+ op2 +") 15px 15px, rgba(48, 173, 67, "+ op3 +")20px 20px, rgba(48, 173, 67, "+ op4 +") 25px 25px";
-        /* box-shadow: rgba(48, 173, 67, 0.4) 5px 5px, rgba(48, 173, 67, 0.3) 10px 10px, rgba(48, 173, 67, 0.2) 15px 15px, rgba(48, 173, 67, 0.1)20px 20px, rgba(48, 173, 67, 0.05) 25px 25px; */
-        op1 += 0.01;
-        if(op1 >= 0.3){
-            op1 = 0.3;
-            op2 += 0.01;
-            if(op2 >= 0.2){
-                op2 = 0.2;
-                op3 += 0.01;
-                if(op3 >= 0.1){
-                    op3 = 0.1;
-                    op4 += 0.01;
-                    if(op4 >= 0.05){
-                        op4 = 0.05;
-                        clearInterval(inter);
-                        func2();
+let src = "";
+function func1(event){
+    if(src == ""){
+        src = event.target.id;
+        console.log(src);
+        inter = setInterval(() => {
+            if(src == "frontend")document.getElementById("frontend").style.boxShadow = "rgba(48, 173, 67, "+ op +") 5px 5px, rgba(48, 173, 67, "+ op1 +") 10px 10px, rgba(48, 173, 67, "+ op2 +") 15px 15px, rgba(48, 173, 67, "+ op3 +")20px 20px, rgba(48, 173, 67, "+ op4 +") 25px 25px";
+            if(src == "backend")document.getElementById("backend").style.boxShadow = "rgba(48, 173, 67, "+ op +") 5px 5px, rgba(48, 173, 67, "+ op1 +") 10px 10px, rgba(48, 173, 67, "+ op2 +") 15px 15px, rgba(48, 173, 67, "+ op3 +")20px 20px, rgba(48, 173, 67, "+ op4 +") 25px 25px";
+            /* box-shadow: rgba(48, 173, 67, 0.4) 5px 5px, rgba(48, 173, 67, 0.3) 10px 10px, rgba(48, 173, 67, 0.2) 15px 15px, rgba(48, 173, 67, 0.1)20px 20px, rgba(48, 173, 67, 0.05) 25px 25px; */
+            op += 0.01;
+            if(op >= 0.4){
+                op = 0.4;
+                op1 += 0.01;
+                if(op1 >= 0.3){
+                    op1 = 0.3;
+                    op2 += 0.01;
+                    if(op2 >= 0.2){
+                        op2 = 0.2;
+                        op3 += 0.01;
+                        if(op3 >= 0.1){
+                            op3 = 0.1;
+                            op4 += 0.01;
+                            if(op4 >= 0.05){
+                                op4 = 0.05;
+                                src = "";
+                                clearInterval(inter);
+                            }
+                        }
                     }
                 }
             }
-        }
-    }, 30);  
+            
+        }, 5);  
+    }
+
 }
-function func2(){
-    inter2 = setInterval(() => {
-        /* box-shadow: rgba(48, 173, 67, 0.4) 5px 5px, rgba(48, 173, 67, 0.3) 10px 10px, rgba(48, 173, 67, 0.2) 15px 15px, rgba(48, 173, 67, 0.1)20px 20px, rgba(48, 173, 67, 0.05) 25px 25px; */
-        document.getElementById("frontend").style.boxShadow = "rgba(48, 173, 67, 0.4) 5px 5px, rgba(48, 173, 67, "+ op1 +") 10px 10px, rgba(48, 173, 67, "+ op2 +") 15px 15px, rgba(48, 173, 67, "+ op3 +")20px 20px, rgba(48, 173, 67, "+ op4 +") 25px 25px";
-        op4 -= 0.01;
-        if(op4 <= 0){
-            op4 = 0;
-            op3 -= 0.01;
-            if(op3 <= 0){
-                op3 = 0;
-                op2 -= 0.01;
-                if(op2 <= 0){
-                    op2 = 0;
-                    op1 -= 0.01;
-                    if(op1 <= 0){
-                        op1 = 0;
-                        clearInterval(inter2);
-                        func1();
+
+function func2(event){
+    if(src == ""){
+        src = event.target.id;
+        console.log(src);
+        inter2 = setInterval(() => {
+            /* box-shadow: rgba(48, 173, 67, 0.4) 5px 5px, rgba(48, 173, 67, 0.3) 10px 10px, rgba(48, 173, 67, 0.2) 15px 15px, rgba(48, 173, 67, 0.1)20px 20px, rgba(48, 173, 67, 0.05) 25px 25px; */
+            op4 -= 0.01;
+            if(op4 <= 0){
+                op4 = 0;
+                op3 -= 0.01;
+                if(op3 <= 0){
+                    op3 = 0;
+                    op2 -= 0.01;
+                    if(op2 <= 0){
+                        op2 = 0;
+                        op1 -= 0.01;
+                        if(op1 <= 0){
+                            op1 = 0;
+                            op -= 0.01;
+                            if(op <= 0){
+                                op = 0;
+                                src = "";
+                                clearInterval(inter2);
+                            }
+                        }
                     }
                 }
             }
-        }
-    }, 30);  
+            if(src == "frontend")document.getElementById("frontend").style.boxShadow = "rgba(48, 173, 67, "+ op +") 5px 5px, rgba(48, 173, 67, "+ op1 +") 10px 10px, rgba(48, 173, 67, "+ op2 +") 15px 15px, rgba(48, 173, 67, "+ op3 +")20px 20px, rgba(48, 173, 67, "+ op4 +") 25px 25px";
+            if(src == "backend")document.getElementById("backend").style.boxShadow = "rgba(48, 173, 67, "+ op +") 5px 5px, rgba(48, 173, 67, "+ op1 +") 10px 10px, rgba(48, 173, 67, "+ op2 +") 15px 15px, rgba(48, 173, 67, "+ op3 +")20px 20px, rgba(48, 173, 67, "+ op4 +") 25px 25px";
+        }, 5);  
+    }
+
 }
-function func12(){
-    inter3 = setInterval(() => {
-        document.getElementById("backend").style.boxShadow = "rgba(48, 173, 67, 0.4) 5px 5px, rgba(48, 173, 67, "+ op1 +") 10px 10px, rgba(48, 173, 67, "+ op2 +") 15px 15px, rgba(48, 173, 67, "+ op3 +")20px 20px, rgba(48, 173, 67, "+ op4 +") 25px 25px";
-        /* box-shadow: rgba(48, 173, 67, 0.4) 5px 5px, rgba(48, 173, 67, 0.3) 10px 10px, rgba(48, 173, 67, 0.2) 15px 15px, rgba(48, 173, 67, 0.1)20px 20px, rgba(48, 173, 67, 0.05) 25px 25px; */
-        op1 += 0.01;
-        if(op1 >= 0.3){
-            op1 = 0.3;
-            op2 += 0.01;
-            if(op2 >= 0.2){
-                op2 = 0.2;
-                op3 += 0.01;
-                if(op3 >= 0.1){
-                    op3 = 0.1;
-                    op4 += 0.01;
-                    if(op4 >= 0.05){
-                        op4 = 0.05;
-                        clearInterval(inter3);
-                        func22();
-                    }
-                }
-            }
-        }
-    }, 30);  
-}
-function func22(){
-    inter4 = setInterval(() => {
-        /* box-shadow: rgba(48, 173, 67, 0.4) 5px 5px, rgba(48, 173, 67, 0.3) 10px 10px, rgba(48, 173, 67, 0.2) 15px 15px, rgba(48, 173, 67, 0.1)20px 20px, rgba(48, 173, 67, 0.05) 25px 25px; */
-        document.getElementById("backend").style.boxShadow = "rgba(48, 173, 67, 0.4) 5px 5px, rgba(48, 173, 67, "+ op1 +") 10px 10px, rgba(48, 173, 67, "+ op2 +") 15px 15px, rgba(48, 173, 67, "+ op3 +")20px 20px, rgba(48, 173, 67, "+ op4 +") 25px 25px";
-        op4 -= 0.01;
-        if(op4 <= 0){
-            op4 = 0;
-            op3 -= 0.01;
-            if(op3 <= 0){
-                op3 = 0;
-                op2 -= 0.01;
-                if(op2 <= 0){
-                    op2 = 0;
-                    op1 -= 0.01;
-                    if(op1 <= 0){
-                        op1 = 0;
-                        clearInterval(inter4);
-                        func12();
-                    }
-                }
-            }
-        }
-    }, 30);  
-}
-func1();
-func12();
